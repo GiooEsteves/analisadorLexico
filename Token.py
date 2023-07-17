c80335ebcbceef5b434c2add37838ba07f038d14
GiooEsteves <100709042+GiooEsteves@users.noreply.github.com>
2023-07-17 16:54:33 -0300

    update tabela de símbolos

--- Token.py
+++ Token.py
@@ -1,5 +1,5 @@
 def criar_token(t):
     return {"tag": t}
 
-def toke_to_str(token):
+def token_to_str(token):
     return "" + chr(token["tag"])
--- symbols.py
+++ symbols.py
@@ -1,47 +1,52 @@
-from typing import Optional
 import Lexer
 
-class Env:
-    def __init__(self, n: Optional['Env'] = None):
-        self.table = {}
-        self.prev = n
+def criar_env(n):
+    return {
+        "table": {},
+        "prev": n
+    }
 
-    def put(self, w: Lexer.Token, i: Lexer.Id):
-        self.table[w] = i
+def put(env, w, i):
+    env["table"][w] = i
 
-    def get(self, w: Lexer.Token) -> Optional[Lexer.Id]:
-        e = self
-        while e is not None:
-            found = e.table.get(w)
-            if found is not None:
-                return found
-            e = e.prev
+def get(env, w):
+    e = env
+    while e is not None:
+        found = e["table"].get(w)
+        if found is not None:
+            return found
+        e = e["prev"]
+    return None
+
+types = {
+    "int": 4,
+    "float": 8,
+    "char": 1,
+    "bool": 1
+}
+
+def is_numeric(p):
+    return p in types
+
+def max_type(p1, p2):
+    if not is_numeric(p1) or not is_numeric(p2):
         return None
+    elif p1 == "float" or p2 == "float":
+        return "float"
+    elif p1 == "int" or p2 == "int":
+        return "int"
+    else:
+        return "char"
 
-class Type(Lexer.Word):
-    def __init__(self, s, tag, w):
-        super().__init__(s, tag)
-        self.width = w
+def criar_array(sz, p):
+    array = {
+        "lexeme": "[]",
+        "tag": Lexer.Tag.INDEX,
+        "width": sz * p["width"],
+        "size": sz,
+        "of": p
+    }
+    return array
 
-        Int = Type("int", Lexer.Tag.BASIC, 4)
-        Float = Type("float", Lexer.Tag.BASIC, 8)
-        Char = Type("char", Lexer.Tag.BASIC, 1)
-        Bool = Type("bool", Lexer.Tag.BASIC, 1)
-
-    @staticmethod
-    def numeric(p):
-        if p == Type.Char or p == Type.Int or p == Type.Float:
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def max(p1, p2):
-        if not Type.numeric(p1) or not Type.numeric(p2):
-            return None
-        elif p1 == Type.Float or p2 == Type.Float:
-            return Type.Float
-        elif p1 == Type.Int or p2 == Type.Int:
-            return Type.Int
-        else:
-            return Type.Char
+def array_to_str(array):
+    return f"[{array['size']}]{array['of']}"
